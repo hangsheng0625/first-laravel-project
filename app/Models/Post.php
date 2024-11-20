ab173d381c2aa74852e1331a2dc7c3e564b3e53e
hlia0024 <hlia0024@student.monash.edu>
2024-11-20 15:42:29 +0800

    Here is to decide which attribute can be mass-assignable (video 22)

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,6 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
-{
-    //
+{   
+    //This is to determine which attribute to be mass assignable
+    protected $fillable = ['title', 'excerpt', 'content', 'published_at'];
+
+    //This is to determine which attribute to not be mass assignable
+    // protected $guarded = ["id"];
 }
