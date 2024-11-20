18477626eb8fdcaa479472e536b0b595edd894ba
hlia0024 <hlia0024@student.monash.edu>
2024-11-20 16:41:49 +0800

    Here is create a category in the database, so that there is a relationship between a post and cateogory (video 24)

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,8 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {   
     //This is to determine which attribute to be mass assignable
-    protected $fillable = ['title', 'excerpt', 'content', 'published_at'];
+    protected $fillable = ['title', 'excerpt', 'content', 'published_at', 'category_id'];
 
     //This is to determine which attribute to not be mass assignable
     // protected $guarded = ["id"];
+
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
 }
