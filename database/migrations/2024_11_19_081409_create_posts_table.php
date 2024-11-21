5ec4eb08a0682b7664e51603a75c7084eab86871
hlia0024 <hlia0024@student.monash.edu>
2024-11-21 12:46:56 +0800

    Here is create a postfactory and categoryfactory so that we can randomly generate their content now, in the dbseeding, look at the comment.

--- database/migrations/2024_11_19_081409_create_posts_table.php
+++ database/migrations/2024_11_19_081409_create_posts_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('title');
             $table->text('excerpt');
             $table->text('content');
+            $table->string('slug'); 
             $table->timestamps();
             $table->timestamp('published_at')->nullable();
         });
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,48 +15,59 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {   
-        User::truncate();
-        Category::truncate();
-        Post::truncate();
+        // User::truncate();
+        // Category::truncate();
+        // Post::truncate();
+        
+        // Now with this code, when we generate a post, it will also generate the user and category automatically since inside the PostFactory we use
+        // User::factory() and Category::factory() to generate the user and category.
+        Post::factory()->create();
 
-        $user = User::factory()->create();
+        // $user = User::factory()->create();
 
         // User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
 
-        $persona = Category::create([
-            'name' => 'Persona',
-            'slug' => 'personal',
-        ]);
+        // $persona = Category::create([
+        //     'name' => 'Persona',
+        //     'slug' => 'personal',
+        // ]);
 
-        $family = Category::create([
-            'name' => 'Family',
-            'slug' => 'family',
-        ]);
+        // $family = Category::create([
+        //     'name' => 'Family',
+        //     'slug' => 'family',
+        // ]);
 
-        $work = Category::create([
-            'name' => 'Work',
-            'slug' => 'work',
-        ]);
+        // $work = Category::create([
+        //     'name' => 'Work',
+        //     'slug' => 'work',
+        // ]);
 
-        Post::create([
-            'title' => 'My Family Post',
-            'category_id' => $family->id,
-            'user_id'=> $user->id,
-            'excerpt' => 'This is my first post with Laravel',
-            'content' => 'This is my first post with Laravel. I am so excited to learn Laravel.',
-        ]);
+        // Post::create([
+        //     'title' => 'My Family Post',
+        //     'category_id' => $family->id,
+        //     'user_id'=> $user->id,
+        //     'excerpt' => 'This is my first post with Laravel',
+        //     'content' => 'This is my first post with Laravel. I am so excited to learn Laravel.',
+        // ]);
 
-        Post::create([
-            'title' => 'My Work Post',
-            'category_id' => $work->id,
-            'user_id'=> $user->id,
-            'excerpt' => 'This is my first post with Laravel',
-            'content' => 'This is my first post with Laravel. I am so excited to learn Laravel.',
-        ]);
+        // Post::create([
+        //     'title' => 'My Work Post',
+        //     'category_id' => $work->id,
+        //     'user_id'=> $user->id,
+        //     'excerpt' => 'This is my first post with Laravel',
+        //     'content' => 'This is my first post with Laravel. I am so excited to learn Laravel.',
+        // ]);
 
+        // This is to show how we can randomly generate multiple posts for the same author
+        // $user = User::factory()->create([
+        //     'name' => 'Hang Sheng'
+        // ]);
 
+        // Post::factory(5)->create([
+        //     'user_id' => $user->id,
+        // ]);
     }
 }
