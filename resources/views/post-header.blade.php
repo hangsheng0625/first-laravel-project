44d3e765bed0530aabda367318eb857f37021244
hlia0024 <hlia0024@student.monash.edu>
2024-11-22 16:10:24 +0800

    Here is create multiple components file to make the post-header more cleaner, for instance the dropdow-item is used to show the available options, and drop-arrow is used to show the arrow, and dropdown is used to handle when the user clicks on it and shows the content

--- resources/views/post-header.blade.php
+++ resources/views/post-header.blade.php
@@ -12,10 +12,23 @@
 
     <div class="space-y-2 lg:space-y-0 lg:space-x-4 mt-8">
         <div class="relative lg:inline-flex bg-gray-100 rounded-xl">
-            <!-- Move the @click.away here -->
             <x-dropdown>
-                
+                <x-slot name="trigger">
+                    <button class="py-2 pl-3 pr-9 text-sm font-semibold w-full lg:w-32 text-left flex lg:inline-flex">
+                        {{ isset($currentCategory) ? ucwords($currentCategory->name) : 'Categories' }}
+                        
+                        <x-down-arrow class="absolute pointer-events-none" style="right: 12px;" />
+                    </button>
+                </x-slot>
+
+                @foreach($categories as $category)
+                    <x-dropdown-item href="/categories/{{ $category->slug }}">
+                        {{ ucwords($category->name) }}
+                    </x-dropdown-item>
+                @endforeach
+            </x-dropdown>
         </div>
+    
 
         <!-- Other Filters -->
         <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl">
